68ac8b2e0d2e95ac139cf3cb0ea7fafb98e8b1fe
Ian Chen <ichen@osrfoundation.org>
2013-08-07 14:29:41 -0700

    Get things working

--- gz3d/build/gz3d.js
+++ gz3d/build/gz3d.js
@@ -5,12 +5,13 @@ var GZ3D = GZ3D || {
 
 GZ3D.Scene = function()
 {
+  this.Init();
 };
 
 
 GZ3D.Scene.prototype.Init = function()
 {
-  var scene = new THREE.Scene();
+  this.scene = new THREE.Scene();
   // scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
   var geometry = new THREE.CylinderGeometry( 0, 1, 3, 4, 1 );
   var material =  new THREE.MeshLambertMaterial(
@@ -21,16 +22,63 @@ GZ3D.Scene.prototype.Init = function()
   mesh.position.z = 0;
   mesh.updateMatrix();
   mesh.matrixAutoUpdate = false;
-  scene.add( mesh );
+  this.scene.add( mesh );
+
+  this.renderer = new THREE.WebGLRenderer({antialias: false });
+  this.renderer.setClearColor(0xcccccc, 1);
+  this.renderer.setSize( window.innerWidth, window.innerHeight);
 
   // lights
   var light = new THREE.DirectionalLight( 0xffffff );
   light.position.set( 1, 1, 1 );
-  scene.add(light);
+  this.scene.add(light);
 
   light = new THREE.AmbientLight( 0x222222 );
-  scene.add(light);
+  this.scene.add(light);
 
   // grid
-  scene.add(new THREE.GridHelper( 10, 1 ));
+  this.scene.add(new THREE.GridHelper( 10, 1 ));
+
+  this.camera = new THREE.PerspectiveCamera(
+      60, window.innerWidth / window.innerHeight, 1, 1000 );
+  this.camera.position.z = 5;
+  this.camera.position.y = 5;
+  this.camera.lookAt(0, 0, 0);
+
+  this.controls = new THREE.TrackballControls(this.camera);
+  this.controls.rotateSpeed = 1.0;
+  this.controls.zoomSpeed = 1.2;
+  this.controls.panSpeed = 0.8;
+  this.controls.noZoom = false;
+  this.controls.noPan = false;
+  this.controls.staticMoving = true;
+  this.controls.dynamicDampingFactor = 0.3;
+  this.controls.keys = [ 65, 83, 68 ];
+
+  this.controls.addEventListener('change', this.Render.call(this));
+
+};
+
+GZ3D.Scene.prototype.GetDomElement = function()
+{
+  return this.renderer.domElement;
+};
+
+
+GZ3D.Scene.prototype.Render = function()
+{
+  this.renderer.render(this.scene, this.camera);
+  this.controls.update();
+};
+
+
+GZ3D.Scene.prototype.SetWindowSize = function(width, height)
+{
+//  this.camera.aspect = window.innerWidth / window.innerHeight;
+  this.camera.aspect = width / height;
+  this.camera.updateProjectionMatrix();
+
+  this.renderer.setSize( width, height);
+  this.controls.handleResize();
+  this.Render();
 };
