23d6d66e996483e6b04f9064ec6a1cb9f7713cba
Hugo Boyer <hugo@osrfoundation.org>
2013-08-29 13:18:32 -0700

    overrides existing models

--- get_local_models.py
+++ get_local_models.py
@@ -15,17 +15,28 @@ def copy_models(src, dst):
             # sys.stdout.write(" copying %s" % model_path)
             print (".", end="")
             dest_dir = os.path.join(dst, os.path.split(model_path)[1] )
+            if os.path.isdir(dest_dir):
+                shutil.rmtree(dest_dir) 
             shutil.copytree(model_path, dest_dir)
         else:
             print (" %s ignored" % model_path)
         
 
 dest_dir = sys.argv[1]
+
+
 print("copying local models to %s" % dest_dir)
-gazebo_path =  os.environ['GAZEBO_MODEL_PATH'].split(':')
-model_paths = [x for x in gazebo_path if os.path.isdir(x)]
-unique_pahts = list(set(model_paths))
- 
+
+unique_paths = None
+try:
+    gazebo_path =  os.environ['GAZEBO_MODEL_PATH'].split(':')
+
+    model_paths = [x for x in gazebo_path if os.path.isdir(x)]
+    unique_pahts = list(set(model_paths))
+except:
+    print ("No local models")
+    exit(0)
+     
 for path in unique_pahts:
     print("\npath: [%s]" % path)
     copy_models(path, dest_dir)
