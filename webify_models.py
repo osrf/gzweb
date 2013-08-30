2bb1340dbc66b2680fa38e76d0d38be3f8974e11
Hugo Boyer <hugo@osrfoundation.org>
2013-08-29 18:50:07 -0700

    moving png textures

--- webify_models.py
+++ webify_models.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import sys
+import shutil
 
 path = sys.argv[1]
 
@@ -15,18 +16,29 @@ for file in files:
   try:
     path, filename = os.path.split(file)
     name, format = filename.split(".")[-2:]
-    if format.lower() in ['tif', 'tga', 'tiff', 'jpeg', 'jpg', 'gif']:
-      dest_path = path.replace('materials/textures', 'meshes')
-      cmd = ["convert", file, "%s/%s.png" % (dest_path, name)]
+    
+  except:
+    continue # not a texture
+  try:    
+    dest_dir = path.replace('materials/textures', 'meshes')
+    dest_path = "%s/%s.png" % (dest_dir, name)
+    if dest_path == file:
+      continue
+    cmd = None
+    if format.lower() in ['tif', 'tga', 'tiff', 'jpeg', 'jpg', 'gif', 'png']:
+      cmd = ['convert', file, dest_path]
+      if format.lower() == 'png':
+        cmd = ['cp', file, dest_dir] 
       print cmd
       subprocess.check_call(cmd)
+          
     if format.lower() in ['dae']:
       sed_cmd = ["sed", "-i", "-e", 's/\.tga/\.png/g', "-e",
           's/\.tiff/\.png/g', "-e", 's/\.tif/\.png/g',
           "-e", 's/\.jpg/\.png/g', "-e", 's/\.jpeg/\.png/g',
           "-e", 's/\.gif/\.png/g', file]
       print sed_cmd
-      subprocess.check_call(sed_cmd)
+      subprocess.check_call(sed_cmd)  
   except Exception, e:
       print "error %s" % e
       raise
