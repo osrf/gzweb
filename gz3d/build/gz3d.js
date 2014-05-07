8d7f5309b648b450d8e2fcef68a367a34945ae01
chapulina <burajiru.no.chapulina@gmail.com>
2014-05-07 12:36:52 -0300

    more efficient bounding box

--- gz3d/build/gz3d.js
+++ gz3d/build/gz3d.js
@@ -3318,7 +3318,39 @@ GZ3D.Scene.prototype.init = function()
   this.radialMenu = new GZ3D.RadialMenu(this.getDomElement());
   this.scene.add(this.radialMenu.menu);
 
-  this.boundingBox = null;
+  // Bounding Box
+  var vertices = [
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, 0),
+
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, 0)
+  ];
+  var boxGeometry = new THREE.Geometry();
+  boxGeometry.vertices.push(
+    vertices[0], vertices[1],
+    vertices[1], vertices[2],
+    vertices[2], vertices[3],
+    vertices[3], vertices[0],
+
+    vertices[4], vertices[5],
+    vertices[5], vertices[6],
+    vertices[6], vertices[7],
+    vertices[7], vertices[4],
+
+    vertices[0], vertices[4],
+    vertices[1], vertices[5],
+    vertices[2], vertices[6],
+    vertices[3], vertices[7]
+  );
+  this.boundingBox = new THREE.Line(boxGeometry,
+      new THREE.LineBasicMaterial({color: 0xffffff}),
+      THREE.LinePieces);
+  this.boundingBox.visible = false;
 };
 
 /**
@@ -4548,7 +4580,7 @@ GZ3D.Scene.prototype.showRadialMenu = function(e)
  */
 GZ3D.Scene.prototype.showBoundingBox = function(model)
 {
-  if (this.boundingBox)
+  if (this.boundingBox.visible)
   {
     if (this.boundingBox.parent === model)
     {
@@ -4561,7 +4593,7 @@ GZ3D.Scene.prototype.showBoundingBox = function(model)
   }
   var box = new THREE.Box3();
   // w.r.t. world
-  box.setFromObject( model );
+  box.setFromObject(model);
   // center vertices with object
   box.min.x = box.min.x - model.position.x;
   box.min.y = box.min.y - model.position.y;
@@ -4569,45 +4601,49 @@ GZ3D.Scene.prototype.showBoundingBox = function(model)
   box.max.x = box.max.x - model.position.x;
   box.max.y = box.max.y - model.position.y;
   box.max.z = box.max.z - model.position.z;
-  var vertices = [
-    new THREE.Vector3(),
-    new THREE.Vector3(),
-    new THREE.Vector3(),
-    new THREE.Vector3(),
 
-    new THREE.Vector3(),
-    new THREE.Vector3(),
-    new THREE.Vector3(),
-    new THREE.Vector3()
-  ];
-  vertices[ 0 ].set( box.max.x, box.max.y, box.max.z );
-  vertices[ 1 ].set( box.min.x, box.max.y, box.max.z );
-  vertices[ 2 ].set( box.min.x, box.min.y, box.max.z );
-  vertices[ 3 ].set( box.max.x, box.min.y, box.max.z );
-  vertices[ 4 ].set( box.max.x, box.max.y, box.min.z );
-  vertices[ 5 ].set( box.min.x, box.max.y, box.min.z );
-  vertices[ 6 ].set( box.min.x, box.min.y, box.min.z );
-  vertices[ 7 ].set( box.max.x, box.min.y, box.min.z );
-  var boxGeometry = new THREE.Geometry();
-  boxGeometry.vertices.push(
-    vertices[ 0 ], vertices[ 1 ],
-    vertices[ 1 ], vertices[ 2 ],
-    vertices[ 2 ], vertices[ 3 ],
-    vertices[ 3 ], vertices[ 0 ],
+  var vertex = new THREE.Vector3(box.max.x, box.max.y, box.max.z); // 0
+  this.boundingBox.geometry.vertices[0].copy(vertex);
+  this.boundingBox.geometry.vertices[7].copy(vertex);
+  this.boundingBox.geometry.vertices[16].copy(vertex);
 
-    vertices[ 4 ], vertices[ 5 ],
-    vertices[ 5 ], vertices[ 6 ],
-    vertices[ 6 ], vertices[ 7 ],
-    vertices[ 7 ], vertices[ 4 ],
+  vertex.set(box.min.x, box.max.y, box.max.z); // 1
+  this.boundingBox.geometry.vertices[1].copy(vertex);
+  this.boundingBox.geometry.vertices[2].copy(vertex);
+  this.boundingBox.geometry.vertices[18].copy(vertex);
 
-    vertices[ 0 ], vertices[ 4 ],
-    vertices[ 1 ], vertices[ 5 ],
-    vertices[ 2 ], vertices[ 6 ],
-    vertices[ 3 ], vertices[ 7 ]
-  );
-  this.boundingBox = new THREE.Line(boxGeometry,
-      new THREE.LineBasicMaterial( { color: 0xffffff } ),
-      THREE.LinePieces);
+  vertex.set(box.min.x, box.min.y, box.max.z); // 2
+  this.boundingBox.geometry.vertices[3].copy(vertex);
+  this.boundingBox.geometry.vertices[4].copy(vertex);
+  this.boundingBox.geometry.vertices[20].copy(vertex);
+
+  vertex.set(box.max.x, box.min.y, box.max.z); // 3
+  this.boundingBox.geometry.vertices[5].copy(vertex);
+  this.boundingBox.geometry.vertices[6].copy(vertex);
+  this.boundingBox.geometry.vertices[22].copy(vertex);
+
+  vertex.set(box.max.x, box.max.y, box.min.z); // 4
+  this.boundingBox.geometry.vertices[8].copy(vertex);
+  this.boundingBox.geometry.vertices[15].copy(vertex);
+  this.boundingBox.geometry.vertices[17].copy(vertex);
+
+  vertex.set(box.min.x, box.max.y, box.min.z); // 5
+  this.boundingBox.geometry.vertices[9].copy(vertex);
+  this.boundingBox.geometry.vertices[10].copy(vertex);
+  this.boundingBox.geometry.vertices[19].copy(vertex);
+
+  vertex.set(box.min.x, box.min.y, box.min.z); // 6
+  this.boundingBox.geometry.vertices[11].copy(vertex);
+  this.boundingBox.geometry.vertices[12].copy(vertex);
+  this.boundingBox.geometry.vertices[21].copy(vertex);
+
+  vertex.set(box.max.x, box.min.y, box.min.z); // 7
+  this.boundingBox.geometry.vertices[13].copy(vertex);
+  this.boundingBox.geometry.vertices[14].copy(vertex);
+  this.boundingBox.geometry.vertices[23].copy(vertex);
+
+  this.boundingBox.geometry.verticesNeedUpdate = true;
+
   // rotate the box back to the world
   var modelRotation = new THREE.Matrix4();
   modelRotation.extractRotation(model.matrixWorld);
@@ -4615,6 +4651,8 @@ GZ3D.Scene.prototype.showBoundingBox = function(model)
   modelInverse.getInverse(modelRotation);
   this.boundingBox.quaternion.setFromRotationMatrix(modelInverse);
   this.boundingBox.name = 'boundingBox';
+  this.boundingBox.visible = true;
+
   // Add box as model's child
   model.add(this.boundingBox);
 };
@@ -4624,12 +4662,11 @@ GZ3D.Scene.prototype.showBoundingBox = function(model)
  */
 GZ3D.Scene.prototype.hideBoundingBox = function()
 {
-  while (this.scene.getObjectByName('boundingBox',true))
+  if(this.boundingBox.parent)
   {
-    var box = this.scene.getObjectByName('boundingBox',true);
-    box.parent.remove(box);
+    this.boundingBox.parent.remove(this.boundingBox);
   }
-  this.boundingBox = null;
+  this.boundingBox.visible = false;
 };
 
 /**
