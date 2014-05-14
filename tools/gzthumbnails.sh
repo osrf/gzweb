1c9f8c25133873f87a959d0dacfc91a17cfd1ab6
chapulina <burajiru.no.chapulina@gmail.com>
2014-05-14 16:05:25 -0300

    transparent and cropped thumbnails

--- tools/gzthumbnails.sh
+++ tools/gzthumbnails.sh
@@ -8,6 +8,17 @@ do
     dir=${dir%*/}
     echo "Creating thumbnail for ${dir##*/}"
     rm -rf $DIR/../http/client/assets/${dir##*/}/thumbnails
-    gzserver -s libModelPropShop.so worlds/blank.world --propshop-save "$DIR/../http/client/assets/${dir##*/}/thumbnails" --propshop-model "$DIR/../http/client/assets/${dir##*/}/model.sdf"
-    #read -n 1 -s
+    if [[ -f $DIR/../http/client/assets/${dir##*/}/model.sdf ]]; then
+	# generate thumbnails with green bg
+        gzserver -s libModelPropShop.so $DIR/green.world --propshop-save "$DIR/../http/client/assets/${dir##*/}/thumbnails" --propshop-model "$DIR/../http/client/assets/${dir##*/}/model.sdf"
+	# make green bg transparent
+	convert $DIR/../http/client/assets/${dir##*/}/thumbnails/1.png -fuzz 30% -transparent '#00ff00' $DIR/../http/client/assets/${dir##*/}/thumbnails/0.png
+	# crop transparent ends
+	convert $DIR/../http/client/assets/${dir##*/}/thumbnails/0.png -trim $DIR/../http/client/assets/${dir##*/}/thumbnails/0.png
+	# add shadow
+	convert -background none -fill black \
+              $DIR/../http/client/assets/${dir##*/}/thumbnails/0.png \
+          \( +clone -background black  -shadow 100x10+0+0 \) +swap \
+          -background none   -layers merge +repage  $DIR/../http/client/assets/${dir##*/}/thumbnails/0.png
+    fi
 done
