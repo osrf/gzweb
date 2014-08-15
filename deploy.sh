9791c6829345169a797adc7c291c86c5288a5391
Ian Chen <ichen@osrfoundation.org>
2014-08-15 13:05:35 -0700

    find imagemagick in cmake, and check for errors in deploy

--- deploy.sh
+++ deploy.sh
@@ -66,12 +66,28 @@ rm -rf build
 mkdir build
 cd build
 
+
+# Run cmake and check for the exit code
 cmake ..
+
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+  echo There are cmake errors, exiting.
+  exit 1
+fi
+
+# continue building if cmake is happy
 make -j 8
 
 cd ../gzbridge
 $DIR/node_modules/.bin/node-gyp configure
 $DIR/node_modules/.bin/node-gyp build -d
+
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+  echo There are node-gyp build errors, exiting.
+  exit 1
+fi
 
 cd $DIR
 
@@ -81,7 +97,7 @@ then
   # Temporal directory for the repository
     TMP_DIR=`mktemp -d`
     cd $TMP_DIR
-  
+
   # If no arg given then download from gazebo_models repo
   if [[ -z $LOCAL ]]
   then
