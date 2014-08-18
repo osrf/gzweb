0e4c3c0fc73e80be96865222c0bae66d77a87318
chapulina <burajiru.no.chapulina@gmail.com>
2014-08-18 17:27:20 -0300

    cross-browser touch detection for CSS

--- gz3d/src/gzgui.js
+++ gz3d/src/gzgui.js
@@ -9,6 +9,7 @@ var emUnits = function(value)
     };
 
 var isTouchDevice = 'ontouchstart' in window || 'onmsgesturechange' in window;
+
 var isWideScreen = function()
     {
       return $(window).width() / emUnits(1) > 35;
@@ -175,6 +176,11 @@ var modelList =
 $(function()
 {
   //Initialize
+  if ('ontouchstart' in window || 'onmsgesturechange' in window)
+  {
+    $('body').addClass('isTouchDevice');
+  }
+
   // Toggle items
   $('#view-collisions').buttonMarkup({icon: 'false'});
   $('#snap-to-grid').buttonMarkup({icon: 'false'});
