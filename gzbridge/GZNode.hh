14983ffcc5020082296b69ff513d46296876e7bf
Ian Chen <ichen@osrfoundation.org>
2016-08-29 17:00:19 -0700

    Backout a1a872f

---
+++ gzbridge/GZNode.hh
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2013 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef _GZNODE_HH_
+#define _GZNODE_HH_
+
+#include <node.h>
+#include <string>
+#include <vector>
+
+namespace gzweb
+{
+  class GazeboInterface;
+
+  class GZNode : public node::ObjectWrap
+  {
+    public: static void Init(v8::Handle<v8::Object> exports);
+
+    private: GZNode();
+
+    private: ~GZNode();
+
+    private: static v8::Handle<v8::Value> New(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value> Callback(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value> Request(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value> SetPoseMsgFilterMinimumAge(const
+      v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value> GetPoseMsgFilterMinimumAge(const
+      v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        SetPoseMsgFilterMinimumDistanceSquared(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        GetPoseMsgFilterMinimumDistanceSquared(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        SetPoseMsgFilterMinimumQuaternionSquared(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        SetConnected(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        GetPoseMsgFilterMinimumQuaternionSquared(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        GetMessages(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        LoadMaterialScripts(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        GetIsGzServerConnected(const v8::Arguments& args);
+
+    private: static v8::Handle<v8::Value>
+        GetMaterialScriptsMessage(const v8::Arguments& args);
+
+    private: GazeboInterface* gzIface;
+    private: bool isGzServerConnected;
+
+  };
+}
+
+#endif
---
+++ gzbridge/ws_server.js
@@ -0,0 +1,133 @@
+#!/usr/bin/env node
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var connections = [];
+var materialScriptsMessage = {};
+var addon = require('./build/Debug/gzbridge');
+var gzconnection = new addon.GZNode();
+if (gzconnection.getIsGzServerConnected())
+{
+  gzconnection.loadMaterialScripts('../http/client/assets');
+  gzconnection.setPoseMsgFilterMinimumAge(0.02);
+  gzconnection.setPoseMsgFilterMinimumDistanceSquared(0.00001);
+  gzconnection.setPoseMsgFilterMinimumQuaternionSquared(0.00001);
+
+  console.log('Pose message filter parameters: ');
+  console.log('  minimum seconds between successive messages: ' +
+      gzconnection.getPoseMsgFilterMinimumAge());
+  console.log('  minimum XYZ distance squared between successive messages: ' +
+      gzconnection.getPoseMsgFilterMinimumDistanceSquared());
+  console.log('  minimum Quartenion distance squared between successive messages:'
+      + ' ' + gzconnection.getPoseMsgFilterMinimumQuaternionSquared());
+}
+else
+{
+  materialScriptsMessage = gzconnection.getMaterialScriptsMessage('../http/client/assets');
+}
+
+var isConnected = false;
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(7681, function() {
+    console.log((new Date()) + ' Server is listening on port 7681');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' +
+          request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept(null, request.origin);
+
+    // if gzserver is not connected just send material scripts and status
+    if (!gzconnection.getIsGzServerConnected())
+    {
+      // create error status and send it
+      var statusMessage = '{"op":"publish","topic":"~/status","msg":{"status":"error"}}';
+      connection.sendUTF(statusMessage);
+      // send material scripts message
+      connection.sendUTF(materialScriptsMessage);
+      return;
+    }
+
+    connections.push(connection);
+
+    if (!isConnected)
+    {
+      isConnected = true;
+      gzconnection.setConnected(isConnected);
+    }
+
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+        console.log('Received Message: ' + message.utf8Data + ' from ' +
+            request.origin + ' ' + connection.remoteAddress);
+        gzconnection.request(message.utf8Data);
+      }
+      else if (message.type === 'binary') {
+        console.log('Received Binary Message of ' +
+            message.binaryData.length + ' bytes');
+        connection.sendBytes(message.binaryData);
+      }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress +
+            ' disconnected.');
+
+      // remove connection from array
+      var conIndex = connections.indexOf(connection);
+      connections.splice(conIndex, 1);
+
+      // if there is no connection notify server that there is no connected client
+      if (connections.length == 0) {
+        isConnected = false;
+        gzconnection.setConnected(isConnected);
+      }
+    });
+});
+
+if (gzconnection.getIsGzServerConnected())
+{
+  setInterval(update, 10);
+
+  function update()
+  {
+    if (connections.length > 0)
+    {
+      var msgs = gzconnection.getMessages();
+      for (var i = 0; i < connections.length; ++i)
+      {
+        for (var j = 0; j < msgs.length; ++j)
+        {
+          connections[i].sendUTF(msgs[j]);
+        }
+      }
+    }
+  }
+}
