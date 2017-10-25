13ff0541e83fcbdb65105aee5bcbee82cbfaed5f
Mohmmad Ayman <no.future2e1@gmail.com>
2017-10-25 22:23:50 +0800

    removed white spaces

--- gzbridge/server.js
+++ gzbridge/server.js
@@ -2,15 +2,15 @@
 
 var WebSocketServer = require('websocket').server;
 var http = require('http');
-var fs = require('fs');  
-var path = require('path');  
+var fs = require('fs');
+var path = require('path');
 
 var staticBasePath = './../http/client';
 
-var staticServe = function(req, res) {  
+var staticServe = function(req, res) {
     var fileLoc = path.resolve(staticBasePath);
     fileLoc = path.join(fileLoc, req.url);
-    
+
     fs.readFile(fileLoc, function(err, data) {
         if (err) {
             res.writeHead(404, 'Not Found');
