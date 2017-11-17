dd5354a996a92632648ec27f8a357beff03c8cf2
Mohmmad Ayman <no.future2e1@gmail.com>
2017-11-17 09:03:24 +0800

    renamed gz3dlib to sdfviewer

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
         'gz3d/client/js/include/xml2json.js', 'gz3d/src/gz*.js', '!gz3d/src/gziface.js',
         '!gz3d/src/gzgui.js'
         ],
-        dest: 'gz3d/build/SDFViewer.lib.js'
+        dest: 'gz3d/build/SDFViewer.js'
       },
     },
     jshint: {
