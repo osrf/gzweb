dca3da5f3409737ec4dfa99b2c11a30587a1bdf3
Mohmmad Ayman <no.future2e1@gmail.com>
2017-11-20 09:27:05 +0800

    added a test for sdf viewer

---
+++ gz3d/test/sdfviewer_test.js
@@ -0,0 +1,27 @@
+describe('SDFviewer related tests', function() {
+
+    var scene = new GZ3D.Scene();
+    var sdfparser = new GZ3D.SdfParser(scene);
+
+    describe('Add a model to the scene using custom urls', function() {
+      it('should add a model to the scene and then removes it', function() {
+
+        var urls = ['https://staging-api.ignitionfuel.org/1.0/models/47d0a5c4-5e34-4f95-a1d9-3f00ef86d6f7/files/model.sdf',
+        'https://staging-api.ignitionfuel.org/1.0/models/47d0a5c4-5e34-4f95-a1d9-3f00ef86d6f7/files/meshes/number.dae'];
+
+        sdfparser.usingFilesUrls = true;
+        sdfparser.addUrl(urls[0]);
+        sdfparser.addUrl(urls[1]);
+
+        var obj = sdfparser.loadSDF();
+        scene.add(obj);
+
+        model = scene.getByName('number1');
+
+        expect(model).not.toEqual(undefined);
+        scene.remove(model);
+        model = scene.getByName('number1');
+        expect(model).toEqual(undefined);
+    });
+  });
+});
