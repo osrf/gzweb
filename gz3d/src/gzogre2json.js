eb44d679ca66e456704174f0c91609267f2bbf62
chapulina <burajiru.no.chapulina@gmail.com>
2018-05-15 15:51:28 -0700

    ogre to json with regex

--- gz3d/src/gzogre2json.js
+++ gz3d/src/gzogre2json.js
@@ -4,6 +4,7 @@
 GZ3D.Ogre2Json = function()
 {
   this.materials = {};
+  this.fullJson = {};
 };
 
 /**
@@ -31,9 +32,60 @@ GZ3D.Ogre2Json.prototype.LoadFromUrl = function(_url)
  */
 GZ3D.Ogre2Json.prototype.Parse = function(_str)
 {
-  console.log(_str);
+  if (_str.indexOf('material') !== 0)
+  {
+    console.err('Missing "material" in:');
+    console.err(_str);
+    return false;
+  }
+
+  var str = _str;
+
+  // Remove initial "material "
+  str = str.replace('material ','');
+
+  // Remove leading and trailing whitespaces per line
+  str = str.replace(/^\s+/gm,'');
+  str = str.replace(/\s+\Z/gm,'');
+
+  // Add comma to end of lines that have space
+  str = str.replace(/(.* .*)/g,'$&,');
+
+  // Remove new lines
+  str = str.replace(/\r?\n|\r/g,'');
+
+  // Add key-value separators
+  str = str.replace(/\s/g, ': ');
+  str = str.replace(/{/g, ': {');
+
+  // Add surrounding brackets
+  str = '{' + str + '}';
+
+  // Wrap keys and values with double quotes
+  str = str.replace(/([\w/\.]+)/g, '"$&"');
+
+  // Remove comma from last property in a sequence
+  str = str.replace(/",}/g, '"}');
+
+  // Parse JSON
+  try
+  {
+    this.fullJson = JSON.parse(str);
+  }
+  catch(e)
+  {
+    console.err('Failed to parse JSON. Original string:');
+    console.err(_str);
+    console.err('Modified string:');
+    console.err(str);
+    return false;
+  }
+
+  // Arrange materials array
   this.materials['Beer/Diffuse'] =
   {
     texture: 'beer.png',
   };
+
+  return true;
 };
